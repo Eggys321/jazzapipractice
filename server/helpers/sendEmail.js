2c942f8288cf4ea7f8e3f70ad67b7048d42a5819
eguono imonieroh <imonieroheguono@gmail.com>
2023-12-08 11:38:52 +0100

    fixes

--- server/helpers/sendEmail.js
+++ server/helpers/sendEmail.js
@@ -6,7 +6,9 @@ require("dotenv/config");
 const sendEmail = (options) => {
   const transporter = createTransport({
     host: process.env.EMAIL_SERVICE,
-    port: 587,
+    port: 2525,
+    // port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
